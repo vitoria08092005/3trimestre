1b1896578d500074e071db68884046613d60082a
vitoria08092005 <163002261+vitoria08092005@users.noreply.github.com>
2024-10-21 11:22:00 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,6 +3,12 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     cosole.log(dados);
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
+    
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
@@ -11,4 +17,6 @@ async function vizualizarInformacoesGlobais() {
     container.appendChild(paragrafo)
 }
 vizualizarInformacoesGlobais()
+paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+console.log(paragrafo)
 
