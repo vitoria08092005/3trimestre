d9d13e4e1eacb4c02ee31777f46d23e9305030a8
vitoria08092005 <163002261+vitoria08092005@users.noreply.github.com>
2024-10-28 11:30:00 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,7 +1,44 @@
-async function quantidadeUsuarios() {
+import { getCSS } from "./common.js";
+
+    async function quantidadeUsuarios() {   
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+    const data = [
+      {
+        x: nomeDasRedes,
+        y: quantidadeUsuarios,
+        type: 'bar',
+        marker: {
+          
+      color: getCSS('--primary-color')
+       }
+     }
       
+    ]
+    const layout = {
+      plot_bgcolor: getCSS('--bg-color'),
+      paper_bgcolor: getCSS('--bg-color'),
+      title: {
+        text: 'Redes sociais com mais usuários no mundo',
+        x: 0,
+        font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30
+        }
+      }
+  
+
+      
+    
+    const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data,layout)
+
+}
       console.log(dados)
-  }
+  
