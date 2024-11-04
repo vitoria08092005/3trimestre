5feef6d0e0c8369018f2b6c7ca08d5a8c29d7d69
vitoria08092005 <163002261+vitoria08092005@users.noreply.github.com>
2024-11-04 10:58:31 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,11 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
   };
+  const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+  }
   
-  export {getCSS}
+  export { getCSS, tickConfig }
+  https://github.com/vitoria08092005/3trimestre.git
